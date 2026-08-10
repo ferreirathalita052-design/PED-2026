aae767483c7a1fdecaa174ee5e6cb8bcc6765021
ferreirathalita052-design <ferreirathalita052@gmail.com>
2026-08-10 20:23:45 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int num1, num2, soma;
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%d , &num1");
+    
+    printf("Digite o segundo numero: ");
+    scanf("%d , &num2");
+    
+    soma = num1 + num2;
+    
+    printf("A soma dos numeros e: %d\n", soma);
+    
+    return 0;
+}
