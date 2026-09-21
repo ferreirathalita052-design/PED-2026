03469e2bc1516096f21d4a615144a1497f31aa04
Thalita Lessa <ferreirathalita052@gmail.com>
2026-09-21 19:30:24 -0300

    Add files via upload

---
+++ lista1/main (1).c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    
+    printf("Digite o valor de A: ");
+    scanf("%d", &A);
+    
+    printf("Digite o valor de B: ");
+    scanf("%d", %B);
+    
+    A = A + B;
+    B = A - B;
+    A = A - B;
+    
+    printf("A = %d\n" A);
+    printf("B = %D\n" B);
+
+    return 0;
+}
